0e38ef05657a030d6d8e42d0f1d2c3257ff74947
saatvikix <saatvik.cloud@gmail.com>
2026-09-26 14:37:14 +0530

    async await

--- video-3/04-async-js/03-async-await.js
+++ video-3/04-async-js/03-async-await.js
@@ -1,1 +1,28 @@
 // async await humein promises ke .then() aur .catch() se bachata hai
+
+let pr = new Promise(function(resolve, reject) {
+    setTimeout(()=>{
+        let num = Math.random() * 10;
+
+        if(num > 5) {
+            resolve(num);
+        }
+        else {
+            reject("FUCK YOU!");
+        }
+    }, 1000);
+});
+
+// async keyword helps us use await
+async function getSomething() {
+    try {
+        let val = await pr;  // wait karo pr ka. 
+        console.log("got the value: ", pr);
+    // jab tak pending state me hai, aage ka code nahi chalega
+    }
+    catch(err) {
+        console.log("failure message from promise: ", err);
+    }
+}   
+
+while(true) getSomething();
